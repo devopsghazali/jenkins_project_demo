a361035e0237854adfcc5c1ae1dc5769c7ad2cf7
devops_ghazali <devopsghazali@gmail.com>
2026-03-30 21:51:17 +0530

    Remove duplicate log statement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 document.getElementById('status').innerText = "Hello from the bundled JS file!";
 console.log("Artifact generation in progress...");
 console.log("hello from java world")
+console.log("hello from java world")
